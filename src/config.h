ec1ebc76d5aa4aaa39be7edf0d2ec435702417ac
Anthony Sleck <anthony.sleck@gmail.com>
2023-05-31 18:40:11 -0500

    UPDATES
    View CHNAGE LOG in CONFIG.H

--- src/config.h
+++ src/config.h
@@ -2,9 +2,7 @@
    ESP32 Temperature Monitor
    Description:
      A simple ESP32 Temperature Monitor with Asysnc Web to display
-     information. The data is also written to a SQL Server and
-     an APACHE PHP webpage. There is also a simple PCB layout
-     included to use a NARROW ESP32 dev board. More to come!
+     information. More to come!
    Notes:
    -Coming Soon!
    Contact Info:
@@ -16,6 +14,7 @@
    0.2 - add'd check before post of data to confirm greater than zero; add'd apache files
    0.4 - add'd DHT sensor data; rem'vs sql support
    0.5 - rmv'd DHT sensor; add'd SQL support back; revised all code; add'd debug option
+   0.6 - add'd SEEED Studio XIAO C3; remv'd watchdog timer; add'd delay to SERIAL.BEGIN; add'd mDNS; remv'd REV from /TEMP webpage footer; remv'd APACHE/PCB path folders
    Contact Info:
    email - anthony.sleck@gmail.com
    web - anthony.sleck.us
@@ -26,8 +25,8 @@
 #define config_h
 
 // system versioning
-#define VER "0.5"
-#define VER_BUILD "042023"
+#define VER "0.6"
+#define VER_BUILD "053123"
 #define email "anthony.sleck@gmail.com"
 #define firmwareLink "https://github.com/anthonysleck/"
 
@@ -37,6 +36,7 @@
 #include <AsyncTCP.h>
 #include <DallasTemperature.h>
 #include <ESPAsyncWebServer.h>
+#include <ESPmDNS.h>
 #include <HTTPClient.h>
 #include <OneWire.h>
 #include <SPI.h>
@@ -45,7 +45,7 @@
 #include <WiFiClient.h>
 
 // debugging
-#define DEBUG 0     // set to "0" to disable serial debugging
+#define DEBUG 1     // set to "0" to disable serial debugging
 
 #if DEBUG == 1
 #define debug(x) Serial.print(x)
@@ -55,32 +55,18 @@
 #define debugln(x)
 #endif
 
-
 // server configs
 AsyncWebServer server(80);
 
-// post data timer
-unsigned long startPostMillis;
-const unsigned long periodPost = 300000;     // the time in ms the system will post data to the SQL server
-
-//post data to WWW
-const char* serverName = "http://********/post-data.php";
-String apiKeyValue = "tPmAT5Ab3j7F9";
-
 // temperature
-#define ONE_WIRE_BUS 4  // set to sensor port; GPIO
+#define ONE_WIRE_BUS 5  // set to sensor port; GPIO
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 String ds_tempf;
 
-// wd timer
-unsigned long startPostMillis;
-const unsigned long periodPost = 300000; // the time in ms
-#define WATCHDOG_TIMEOUT_S 15            // enter time in sec
-hw_timer_t *watchDogTimer = NULL;
-
 // WiFi settings
-const char *ssid = "********";             // enter your SSID here
-const char *password = "********"; // enter password here
+const char* host = "ESPTempMonitor";  // change to the desired DNS name
+const char *ssid = "********";        // enter your SSID here
+const char *password = "********";    // enter password here
 
 #endif
--- src/esp32temperaturemonitor.cpp
+++ src/esp32temperaturemonitor.cpp
@@ -12,7 +12,7 @@ String readDSTemperature()
   }
   else
   {
-    //debugln(espTemperature);
+    // debugln(espTemperature);
     return String(espTemperature);
   }
 }
@@ -30,27 +30,6 @@ String processor(const String &var)
   return String();
 }
 
-void IRAM_ATTR watchDogInterrupt()
-{
-  debugln("reboot");
-  ESP.restart();
-}
-
-void watchDogRefresh()
-{
-  timerWrite(watchDogTimer, 0);   //reset timer
-}
-
-void startWatchdogTimer()
-{
-  debugln("Starting watchdog timer!");
-  watchDogTimer = timerBegin(2, 80, true);
-  timerAttachInterrupt(watchDogTimer, &watchDogInterrupt, true);
-  timerAlarmWrite(watchDogTimer, WATCHDOG_TIMEOUT_S * 1000000, false);
-  timerAlarmEnable(watchDogTimer);
-  debugln("Watchdog timer started!");
-}
-
 void temperatureSensors()
 {
   sensors.requestTemperatures();
@@ -58,23 +37,20 @@ void temperatureSensors()
   if (ds_temperature > 0)
   {
     ds_tempf = String(ds_temperature);
-    
+
     // print current temperature data
     debugln("");
     debug("Current Temperature is: ");
     debug(ds_tempf);
     debugln("°F");
   }
-
-  // reset watchdog timer
-  watchDogRefresh();
 }
 
 void rootServer()
 {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send_P(200, "text/html", index_html, processor); });
-             
+
   server.on("/temp", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send_P(200, "text/html", temp_html, processor); });
 
@@ -87,55 +63,14 @@ void rootServer()
   debugln("HTTP server started");
 }
 
-void postData()
-{
-  // get current temperature data
-  temperatureSensors();
-
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    WiFiClient client;
-    HTTPClient http;
-
-    http.begin(client, serverName);
-
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(ds_tempf) + "";
-    debug("httpRequestData: ");
-    debugln(httpRequestData);
-
-    int httpResponseCode = http.POST(httpRequestData);
-
-    if (httpResponseCode > 0)
-    {
-      debug("HTTP Response code: ");
-      debugln(httpResponseCode);
-    }
-    else
-    {
-      debug("Error code: ");
-      debugln(httpResponseCode);
-    }
-    http.end();
-  }
-  else
-  {
-    debugln("WiFi Disconnected");
-  }
-
-  // reset watchdog timer
-  watchDogRefresh();
-}
-
 void setup()
 {
-  delay(5000);
-
   // start serial
   Serial.begin(115200);
+  Serial.setTxTimeoutMs(0);
+  delay(5000);
 
-  // print sketch information
+  // sketch information
   debugln("Created by Anthony Sleck");
   debugln("Email at anthony.sleck@gmail.com");
   debug("Version ");
@@ -155,8 +90,8 @@ void setup()
 
   while (WiFi.status() != WL_CONNECTED)
   {
-      delay(1000);
-      debug(".");
+    delay(1000);
+    debug(".");
   }
 
   debugln("");
@@ -165,28 +100,22 @@ void setup()
   debug("IP address: ");
   debugln(WiFi.localIP());
 
+  // mDNS start
+  if (!MDNS.begin(host))
+  {
+    debugln("Error setting up the mDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }    
+  }
+  debugln("mDNS responder started!");
+
   // start server
   rootServer();
-
-  // start watchdog timer
-  startWatchdogTimer();
-
-  // initial data to serial monitor and database
-  postData();
 }
 
 void loop()
 {
-  // post to php-mysql
-  unsigned long currentPostMillis = millis();
-  if (currentPostMillis - startPostMillis >= periodPost)
-  {
-    postData();
-    
-    //save the start millis()
-    startPostMillis = currentPostMillis;
-  }
-
-  // reset watchdog timer
-  watchDogRefresh();
+  // add code to loop here
 }
